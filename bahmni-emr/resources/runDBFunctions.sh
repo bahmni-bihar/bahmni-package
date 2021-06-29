01a8e23a884bbdc0f6531c2fcdc735d7c728a241
Hiren Thacker <hithacker@gmail.com>
2021-06-29 09:42:20 +0530

    Added scripts to run to create dbs with different name on the same server

---
+++ bahmni-emr/resources/runDBFunctions.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -f /etc/bahmni-installer/bahmni.conf ]; then
+. /etc/bahmni-installer/bahmni.conf
+fi
+
+. /etc/openmrs/openmrs.conf
+. /etc/openmrs/bahmni-emr.conf
+
+run_migrations() {
+    echo "Running openmrs liquibase-core-data.xml and liquibase-update-to-latest.xml"
+    ./run-liquibase.sh liquibase-core-data.xml
+    ./run-liquibase.sh liquibase-update-to-latest.xml
+}
+
+mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $OPENMRS_DB_NAME;"
+mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD $OPENMRS_DB_NAME < openmrs_demo_dump.sql
+
+mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $OPENMRS_DB_NAME.* TO '$OPENMRS_DB_USER'@'$OPENMRS_DB_SERVER';"
+
+run_migrations
